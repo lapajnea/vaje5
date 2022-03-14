073a9bf0a319782c79c6ff6ccd82d5390fd97b35
Andreja Lapajne <al9390@student.uni-lj.si>
2022-03-14 11:08:41 +0100

    sprememba

--- vaje5/Program.cs
+++ vaje5/Program.cs
@@ -6,7 +6,8 @@ namespace vaje5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("No");
+            Console.WriteLine("ojoj");
         }
     }
 }
